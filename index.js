e030ec0dd9982bfd2eecabb1efd091240abed53a
Emmanuel Lyimo <esal29t@gmail.com>
2023-03-20 23:52:16 -0700

    modified the index.js file

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 
 // initialize the counter
 let count = 0
-
+let countEL = document.getElementById("count-el")
 // // // listen for clicks on the increment button
 
 // // // increment the count variable when the button is clicked
 
 function increment() {
     count = count + 1;
+    countEL.innerHTML = count
     console.log(count)
 }
 
